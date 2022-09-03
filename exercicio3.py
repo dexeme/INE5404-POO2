0514d2299b3c53e20f3ebb7a47d75a666410a8b6
memataexe <dexe.me12@gmail.com>
2022-09-03 12:13:54 -0300

    Função que recebe input e coloca coeficientes em uma lista

--- exercicio3.py
+++ exercicio3.py
@@ -2,3 +2,11 @@ class Polinomio():
     def __init__(self, coeficientes):
         self.coeficientes = coeficientes
 
+    def entrada():
+        coeficientes = []
+        while True:
+            try:
+                coeficientes.append(int(input('Digite o coeficiente: ')))
+            except ValueError:
+                break
+        return coeficientes
