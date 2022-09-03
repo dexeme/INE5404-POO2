ce94b1efb38056f7cfadef03b9ac61491c2acaca
memataexe <dexe.me12@gmail.com>
2022-09-03 12:14:49 -0300

    Funções multiplicação e soma e polinômios

--- exercicio3.py
+++ exercicio3.py
@@ -10,3 +10,26 @@ class Polinomio():
             except ValueError:
                 break
         return coeficientes
+
+    def grau_do_polinomio(self):
+        return len(self.coeficientes) - 1
+    
+    def somar_polinomios(self, polinomio):
+        if len(self.coeficientes) > len(polinomio.coeficientes):
+            maior = self.coeficientes
+            menor = polinomio.coeficientes
+        else:
+            maior = polinomio.coeficientes
+            menor = self.coeficientes
+        
+        for i in range(len(menor)):
+            maior[i] += menor[i]
+        
+        return maior
+    
+    def multiplicar_polinomios(self, polinomio):
+        resultado = [0] * (len(self.coeficientes) + len(polinomio.coeficientes) - 1)
+        for i in range(len(self.coeficientes)):
+            for j in range(len(polinomio.coeficientes)):
+                resultado[i + j] += self.coeficientes[i] * polinomio.coeficientes[j]
+        return resultado
