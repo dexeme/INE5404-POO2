9f26e5686861b90185403b17732ed9a976f5cbd8
memataexe <dexe.me12@gmail.com>
2022-10-06 15:47:08 -0300

    Commit VPL 4 - Ajuste

--- VPL/VPL 4 - Controle de Chamados/interfaces/controladorChamados.py
+++ VPL/VPL 4 - Controle de Chamados/interfaces/controladorChamados.py
@@ -1,4 +1,4 @@
-from abstractControladorChamados import AbstractControladorChamados
+from interfaces import AbstractControladorChamados
 from tipoChamado import TipoChamado
 from chamado import Chamado
 from datetime import date as Date
@@ -8,3 +8,55 @@ from collections import defaultdict
 
 
 class ControladorChamados(AbstractControladorChamados):
+
+    def __init__(self):
+        self.__chamados = []
+        self.__tipos_chamados = []
+
+    @property
+    def chamados(self):
+        return self.__chamados
+
+    @property
+    def tipoChamados(self):
+        return self.__tipos_chamados
+
+    def totalChamadosPorTipo(self, tipo: TipoChamado) -> int:
+        total = 0
+        for chamado in self.chamados:
+            if chamado.tipo.codigo == tipo.codigo:
+                total += 1
+        return total
+
+    def incluiChamado(self, data: Date, cliente: Cliente,
+                      tecnico: Tecnico, titulo: str,
+                      descricao: str, prioridade: int,
+                      tipo: TipoChamado) -> Chamado:
+        if (isinstance(data, Date) and
+                isinstance(cliente, Cliente) and
+                isinstance(tecnico, Tecnico) and
+                isinstance(tipo, TipoChamado)):
+            jaesta = False
+            for chamado in self.chamados:
+                if (chamado.data == data and
+                        chamado.cliente == cliente and
+                        chamado.tecnico == tecnico and
+                        chamado.tipo == tipo):
+                    jaesta = True
+            if not jaesta:
+                chamado = Chamado(data, cliente,
+                                  tecnico, titulo,
+                                  descricao, prioridade, tipo)
+                self.chamados.append(chamado)
+                return chamado
+
+    def incluiTipoChamado(self, codigo: int,
+                          nome: str, descricao: str) -> TipoChamado:
+        jaesta = False
+        for tipo in self.tipoChamados:
+            if tipo.codigo == codigo:
+                jaesta = True
+        if not jaesta:
+            tipochamado = TipoChamado(codigo, descricao, nome)
+            self.tipoChamados.append(tipochamado)
+            return tipochamado
--- VPL/VPL 4 - Controle de Chamados/interfaces/controladorPessoas.py
+++ VPL/VPL 4 - Controle de Chamados/interfaces/controladorPessoas.py
@@ -1,7 +1,40 @@
-from abstractControladorPessoas import AbstractControladorPessoas
+from interfaces import AbstractControladorPessoas
 from cliente import Cliente
 from tecnico import Tecnico
 
 
 class ControladorPessoas(AbstractControladorPessoas):
+
     def __init__(self):
+        self.__clientes = []
+        self.__tecnicos = []
+
+    @property
+    def clientes(self) -> list:
+        return self.__clientes
+
+    @property
+    def tecnicos(self) -> list:
+        return self.__tecnicos
+
+    def incluiCliente(self, codigo: int, nome: str) -> Cliente:
+        jaesta = False
+        for cliente in self.clientes:
+            if codigo == cliente.codigo:
+                jaesta = True
+                break
+        if not jaesta:
+            cliente = Cliente(nome, codigo)
+            self.__clientes.append(cliente)
+            return cliente
+
+    def incluiTecnico(self, codigo: int, nome: str) -> Tecnico:
+        jaesta = False
+        for tecnico in self.tecnicos:
+            if codigo == tecnico.codigo:
+                jaesta = True
+                break
+        if not jaesta:
+            tecnico = Tecnico(nome, codigo)
+            self.__tecnicos.append(tecnico)
+            return tecnico
